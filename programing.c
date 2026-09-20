296f2c24972f191992bb70ab98c2fd4d74f6852a
HARSHIT <harshitkatiyar738@gmail.com>
2026-09-20 14:53:34 +0530

    add my this first project on opensouce.

---
+++ programing.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+    printf("------------------menu--------------------\n");
+    printf("1.ADDITION\n");
+    printf("2.subtraction\n");
+    printf("3.multiplication\n");
+    printf("4.divide\n");
+    int choice,a,b;
+    printf("enter the choice = ");
+    scanf("%d",&choice);
+    printf("enter the val of a = ");
+    scanf("%d",&a);
+    printf("enter the val of b = ");
+    scanf("%d",&b);
+    switch (choice){
+        case 1:
+        printf("\naddition = %d\n",a+b);
+        break;
+        case 2:
+        printf("subtraction = %d\n",a-b);
+        break;
+        case 3:
+        printf("multiply = %d\n",a*b);
+        break;
+        case 4:
+        printf("divide = %d\n",a/b);
+        break;
+        default:
+        printf("choose from 1 to 4\n");
+        break;
+    }
+    printf("-------------------------best of luck---------------------");
+    return 0;
+}
